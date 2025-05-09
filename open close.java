dff4b69db8ae67678a83f1795ad903457764f5d0
naveen0981 <naveenkumar123hsh@gmail.com>
2025-05-09 21:43:17 +0530

    Create open close.java

---
+++ open close.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+       // for(int i=0;i<=a;i++){
+            if(a%2==0){
+                System.out.println("open");
+            }
+            else{
+                System.out.println("Close");
+            }
+        }
+    }
+            
+
+    
